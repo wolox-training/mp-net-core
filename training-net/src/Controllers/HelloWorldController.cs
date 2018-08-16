feb8a3f673b3ac2700882f8bd01c5cc866ebb128
mar7in91Wolox <martin.perez@wolox.com.ar>
2018-08-16 10:38:01 -0300

    PR issues solved

--- training-net/src/Controllers/HelloWorldController.cs
+++ training-net/src/Controllers/HelloWorldController.cs
@@ -5,17 +5,10 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
-        // GET: /HelloWorld/
-
         public string Index()
         {
             return "This is my default action...";
         }
-
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
         public string Welcome()
         {
             return "This is the Welcome action method...";
