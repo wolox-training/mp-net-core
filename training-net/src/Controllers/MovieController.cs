bfec9e150dd77197aa1b9b1acb221b6b27181823
mar7in91Wolox <martin.perez@wolox.com.ar>
2018-08-23 14:23:34 -0300

    Create in progress....

--- training-net/src/Controllers/MovieController.cs
+++ training-net/src/Controllers/MovieController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using training_net.Models.Views;
 using training_net.Repositories.Database;
 using training_net.Repositories.Interfaces;
@@ -10,10 +12,11 @@ namespace training_net.Controllers
     public class MovieController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        
-        public MovieController(IUnitOfWork unitOfWork)
+        private readonly IHtmlLocalizer<MovieController> _localizer;
+        public MovieController(IUnitOfWork unitOfWork, IHtmlLocalizer<MovieController> localizer)
         {
             this._unitOfWork = unitOfWork;
+            this._localizer = localizer;
         }
 
         public IUnitOfWork UnitOfWork
@@ -21,6 +24,16 @@ namespace training_net.Controllers
             get { return this._unitOfWork; }
         }
 
-        public IActionResult Index() => View();
+        public IActionResult Index()
+        {
+            //ViewData["indexMovieTitle"] = localizer;
+            //@ViewData["indexMoviePar"] = ;      
+            return View();
+        }
+        public IActionResult Create()
+        {
+                              
+            return View();
+        }
     }
 }
