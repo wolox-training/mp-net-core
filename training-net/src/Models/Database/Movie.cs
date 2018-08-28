3f40a8c93f9415dda48ffec02617b34cd02c214c
mar7in91Wolox <martin.perez@wolox.com.ar>
2018-08-28 10:31:49 -0300

    Movie list added

--- training-net/src/Models/Database/Movie.cs
+++ training-net/src/Models/Database/Movie.cs
@@ -7,6 +7,7 @@ namespace training_net.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
