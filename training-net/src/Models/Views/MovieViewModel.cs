ace903423724145c1f40aeebf24eab56c2e398b2
mar7in91Wolox <martin.perez@wolox.com.ar>
2018-08-29 12:56:46 -0300

    Internationalization added on create btn and form

--- training-net/src/Models/Views/MovieViewModel.cs
+++ training-net/src/Models/Views/MovieViewModel.cs
@@ -5,15 +5,10 @@ namespace training_net.Models.Views
 {
   public class MovieViewModel 
     {
-        [Display(Name = "Title")]
         public string Title { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Release Date")]
-
         public DateTime ReleaseDate { get; set; }
-        [Display(Name = "Genre")]
         public string Genre { get; set; }
-         [Display(Name = "Price")]
         public float Price { get; set; }
     }
 }
