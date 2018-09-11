e15b3103e65c7452ff6e1fe62ae702c35750ea91
mar7in91Wolox <martin.perez@wolox.com.ar>
2018-09-11 13:19:16 -0300

    Comment Model migration

---
+++ training-net/src/Migrations/20180911160321_RenameCommentText.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace training_net.Migrations
+{
+    public partial class RenameCommentText : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "User",
+                table: "Comment",
+                newName: "UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "CommentString",
+                table: "Comment",
+                newName: "Text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_UserId",
+                table: "Comment",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comment_UserId",
+                table: "Comment");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Comment",
+                newName: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Comment",
+                newName: "CommentString");
+        }
+    }
+}
--- training-net/src/Models/Database/Comment.cs
+++ training-net/src/Models/Database/Comment.cs
@@ -7,7 +7,7 @@ namespace training_net.Models
     public class Comment
     {
         public int Id { get; set; }
-        public string CommentString { get; set;}
+        public string Text { get; set;}
         public User User { get; set;}
         public Movie Movie { get; set;}
     }
